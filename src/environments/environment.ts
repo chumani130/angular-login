94edb0f7da9a90c6c68acf45c43f105f2d9a802d
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-08-31 16:18:03 +0200

    Create environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBir683G91ub8t_jbWkLP5L-XQU4YgAUso",
+    authDomain: "contactform-ecc64.firebaseapp.com",
+    projectId: "contactform-ecc64",
+    storageBucket: "contactform-ecc64.appspot.com",
+    messagingSenderId: "997125614522",
+    appId: "1:997125614522:web:4ffc46991baf00e1580b83"
+  },
+};
